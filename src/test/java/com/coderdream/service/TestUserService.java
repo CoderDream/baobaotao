9c817d3075f13adf1c176261926bdf7783782fe0
CoderDream <coderdream@gmail.com>
2015-02-05 23:44:30 +0800

    格式化

--- src/test/java/com/coderdream/service/TestUserService.java
+++ src/test/java/com/coderdream/service/TestUserService.java
@@ -17,30 +17,30 @@ import com.coderdream.domain.User;
 @ContextConfiguration({ "classpath:applicationContext.xml" })
 public class TestUserService {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Test
-	public void testHasMatchUser() {
-		boolean b1 = userService.hasMatchUser("admin", "123456");
-		boolean b2 = userService.hasMatchUser("admin", "1111");
-		assertTrue(b1);
-		assertTrue(!b2);
-	}
+    @Test
+    public void testHasMatchUser() {
+        boolean b1 = userService.hasMatchUser("admin", "123456");
+        boolean b2 = userService.hasMatchUser("admin", "1111");
+        assertTrue(b1);
+        assertTrue(!b2);
+    }
 
-	@Test
-	public void testFindUserByUserName() {
-		User user = userService.findUserByUserName("admin");
-		assertEquals(user.getUserName(), "admin");
-	}
+    @Test
+    public void testFindUserByUserName() {
+        User user = userService.findUserByUserName("admin");
+        assertEquals(user.getUserName(), "admin");
+    }
 
-	@Test
-	public void testAddLoginLog() {
-		User user = userService.findUserByUserName("admin");
-		user.setUserId(1);
-		user.setUserName("admin");
-		user.setLastIp("192.168.12.7");
-		user.setLastVisit(new Date());
-		userService.loginSuccess(user);
-	}
+    @Test
+    public void testAddLoginLog() {
+        User user = userService.findUserByUserName("admin");
+        user.setUserId(1);
+        user.setUserName("admin");
+        user.setLastIp("192.168.12.7");
+        user.setLastVisit(new Date());
+        userService.loginSuccess(user);
+    }
 }
